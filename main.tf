9c423fe6b57106bfb0e29ad0fc3f55fb57d39771
Alvaro Rodriguez Gonzalez <alvamige@gmail.com>
2025-06-20 23:03:28 +0200

    TFM-26 | Basic RDS instance (#1)
    * TFM-26 | Basic RDS instance
    
    * Apply terraform formatting
    
    * Multi-AZ false

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "aws_db_instance" "postgres" {
+  identifier        = "public-postgres-db"
+  allocated_storage = 20
+  engine            = "postgres"
+  engine_version    = "15.4"
+  instance_class    = "db.t3.micro"
+  multi_az          = false
+
+  db_name  = "postgres"
+  username = "postgres"
+  password = "postgres"
+
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.postgres_sg.id]
+  skip_final_snapshot    = true
+  publicly_accessible    = true
+
+  tags = {
+    Name = "PostgreSQL RDS"
+  }
+}
